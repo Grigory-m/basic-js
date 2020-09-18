e0bd00dd2c8b3a3ff0456b8e25ff60c377cfa77c
Grisha <gm201212@gmail.com>
2020-09-18 20:09:51 +0300

    refactor: delete comments

--- src/count-cats.js
+++ src/count-cats.js
@@ -13,14 +13,5 @@ module.exports = function countCats( matrix ) {
       if (el === '^^') count++;
     });
   });
-  // let number = 0;
-  //   for (let i = 0; i < matrix.length; i++) {
-  //     for (let j = 0; j < matrix[i].length; j++) {
-  //       if (matrix[i][j] == '^^') {
-  //         number++;
-  //       }
-  //     }      
-  //   }
-  // return number;
   return count;
 };
